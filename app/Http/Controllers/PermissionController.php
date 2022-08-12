924a89d70ab0d1ba0787a9a929544cf846ee8e7b
ktvrt <cutbertomo@gmail.com>
2022-08-11 19:34:43 -0500

    edit permisos

--- app/Http/Controllers/PermissionController.php
+++ app/Http/Controllers/PermissionController.php
@@ -63,9 +63,9 @@ class PermissionController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function edit($id)
+    public function edit(Permission $permission)
     {
-        //
+        return view('permisos.edit', compact('permission'));
     }
 
     /**
