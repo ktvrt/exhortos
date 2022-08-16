50e239ce0f5e1c7a7e0d63eba3a524be5e9878ef
Cutberto Mancilla <cutbertoo@gmail.com>
2022-08-16 10:56:01 -0500

    fix problema de compatibilidad con la bd mysql con los indices en las migraciones

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Pagination\Paginator;
 
 class AppServiceProvider extends ServiceProvider
@@ -24,6 +25,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        //se agrego por problemas de mysql con los indices
+        Schema::defaultStringLength(191);
         //Laravel incluye vistas de paginación creadas con Bootstrap CSS .
         //Para usar estas vistas en lugar de las vistas predeterminadas de Tailwind,
         //puede llamar al método del paginador useBootstrapdentro del bootmétodo
