bec1f4e41597fd048269f07396091b3214028070
ktvrt <cutbertomo@gmail.com>
2022-09-01 10:24:28 -0500

    Personalizamos vista Home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.main', ['activePage' => 'dashboard', 'titlePage' => __('Dashboard')])
+@extends('layouts.main', ['activePage' => 'dashboard', 'titlePage' => __('Exhortos Digitales')])
 
 @section('content')
   <div class="content">
@@ -6,7 +6,33 @@
     <div class="container-fluid">
       <div class="row">
 
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          <div class="mainArticle">
+
+
+              <h1>Exhortos</h1>
+              <p>
+              Son pedidos que realiza una autoridad requirente a otra autoridad requerida por hallarse fuera de su jurisdicción (entiéndase por autoridad requirente y requerida la de un juez), a través de los cuales se solicitan notificaciones, citaciones, intimaciones, interrogatorios, traba de embargos, extradiciones y restitución internacional de menores, etc.
+              </p>
+              <h1>Pasos para la presentación</h1>
+              <ul>
+                <li>
+                  1- Los mismos deben ser solicitados al Juzgado correspondiente en el que se deberá elaborar el exhorto y en el escrito detallar el Juzgado requirente y el Juzgado requerido del país correspondiente, así como la dirección donde se realizará la diligencia respectiva.
+                </li>
+                <li>
+                  2- Para los casos de traslado de demandas deberán acompañarse copias autenticadas de las mismas. Si el país al que se remitirá el exhorto no es de habla hispana, deberá ir acompañado de la traducción correspondiente (traductor matriculado ante la Corte Suprema de Justicia).
+                </li>
+                <li>
+                 3- Para los casos de extradición se deberán acompañar copia de resolución o S.D. donde se ordena la detención preventiva o condena si lo hubiere; también copia del artículo del Código Penal en donde se halla tipi­ficado el delito, así como datos ob­ligatorios del extraditable y demás documentos que crea conveniente el Juzgado.
+                </li>
+              </ul>
+              <h1>Procedimiento para la tramitación</h1>
+              <p>
+              Una vez elaborado el exhorto por el Juzgado, el mismo, a través de una nota, deberá remitir a la Dirección de&nbsp;Cooperación y Asistencia Judicial Internaciona &nbsp;de&nbsp;la Corte solicitando en la misma su diligenciamiento.<br>
+            </p>
+            <p>
+              La Dirección de&nbsp;Cooperación y Asistencia Judicial Internacional&nbsp;encaminará dicho exhorto a través de la Autoridad Central correspondiente, la que a su vez remitirá al país requirente para el trámite correspondiente.
+            </p>
+        </div>
 
       </div>
     </div>
